523475e805b32a02f9bbc7601a3a1a6fab0555cd
ANHONY <mugumyaali@outlook.com>
2025-01-28 19:39:28 +0300

    working on the main activity

--- app/src/main/java/com/example/buyonline/IntroActivity.kt
+++ app/src/main/java/com/example/buyonline/IntroActivity.kt
@@ -10,6 +10,7 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        //hello
 
     }
 }
